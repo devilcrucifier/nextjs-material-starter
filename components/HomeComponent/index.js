95234f7c8619096b78446a4a2a9db6bffefa0533
Amit Kushwaha <amitbvp13@gmail.com>
2020-10-11 22:37:10 +0530

    Added material components

--- components/HomeComponent/index.js
+++ components/HomeComponent/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { withStyles } from "@material-ui/styles";
+import Button from "@material-ui/core/Button";
 
-const HomeComponent = () => (
-  <div>
-    <div>This is Home page</div>
+const styles = theme => ({
+  root: { padding: "5%" },
+  heading: {
+    ...theme.typography.defaultFont,
+    margin: "20px 0"
+  }
+});
+
+const HomeComponent = ({ classes }) => (
+  <div className={classes.root}>
+    <div className={classes.heading}>This is Home page</div>
     <div>
       <Link href="/temp">
-        <a>Go to Temp Page</a>
+        <Button color="primary" variant="contained">
+          Go to Temp Page
+        </Button>
       </Link>
     </div>
   </div>
 );
 
-export default HomeComponent;
+export default withStyles(styles)(HomeComponent);
--- components/TempComponent/index.js
+++ components/TempComponent/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { withStyles } from "@material-ui/styles";
+import Button from "@material-ui/core/Button";
 
-const TempComponent = () => (
-  <div>
-    <div>This is Temp page</div>
+const styles = theme => ({
+  root: { padding: "5%" },
+  heading: {
+    ...theme.typography.defaultFont,
+    margin: "20px 0"
+  }
+});
+
+const TempComponent = ({ classes }) => (
+  <div className={classes.root}>
+    <div className={classes.heading}>This is Temp page</div>
     <div>
       <Link href="/">
-        <a>Go to Home Page</a>
+        <Button color="secondary" variant="contained">
+          Go to Home Page
+        </Button>
       </Link>
     </div>
   </div>
 );
 
-export default TempComponent;
+export default withStyles(styles)(TempComponent);
